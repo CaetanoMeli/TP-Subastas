da84a9c85e9e975f7f029abb5fb567e664c3479e
CaetanoMeli <caetano.riveron@mercadolibre.com>
2021-06-19 21:44:44 -0300

    remove service

--- src/main/java/com/uade/api/controllers/AuctionController.java
+++ src/main/java/com/uade/api/controllers/AuctionController.java
@@ -4,7 +4,6 @@ import com.uade.api.dtos.response.AuctionDetailDTO;
 import com.uade.api.marshallers.AuctionMarshaller;
 import com.uade.api.models.AuctionModel;
 import com.uade.api.services.AuctionService;
-import com.uade.api.services.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("auctions")
 public class AuctionController {
 
-    private final UserService userService;
     private final AuctionService auctionService;
     private final AuctionMarshaller auctionMarshaller;
 
     public AuctionController(
-            UserService userService,
             AuctionService auctionService,
             AuctionMarshaller auctionMarshaller
     ) {
-        this.userService = userService;
         this.auctionService = auctionService;
         this.auctionMarshaller = auctionMarshaller;
     }
